19b730aeb72dbd748aabb66fd17134cf9dd3c7fd
Xiao Tan <tanxiaobuaa@gmail.com>
2023-09-05 17:18:39 +0200

    changed case 2 STL formula

--- unitTest_stl2sTLT_Uni.m
+++ unitTest_stl2sTLT_Uni.m
@@ -6,7 +6,7 @@ clc; clear all;close all;
 % tree1 = sTLTObj(nodeList,parentList);
 % tree1.draw_sTLT()
 
-case_numb = 0;
+case_numb = 2;
 sys_dyn = 'unicycle';
 vRange = [-1 1];
 wMax = 1;
@@ -17,7 +17,7 @@ switch case_numb
     case 0
         % trivial case STL
         st = 'F[0,15] mu1';
-        predicateList = {'mu1','mu2', 'mu3'}';
+        predicateList = {'mu1','mu2', 'mu3'}'; R1.rep = 'circular';
         R1.c = [-4;-4]; R1.r = 1; R1.obstacle = false;
         
         % Create a containers.Map
@@ -33,9 +33,9 @@ switch case_numb
         % STL
         st = 'F[0,15] G[2,10] mu1 Vee F[0,15](G[0,10] mu2 Wedge F[5,10] mu3 )';
         predicateList = {'mu1','mu2', 'mu3'}';
-        R1.c = [-4;-4]; R1.r = 1; R1.obstacle = false;
-        R2.c = [4;0];R2.r = 4;R2.obstacle = false;
-        R3.c = [1;-4];R3.r = 2;R3.obstacle = false;
+        R1.c = [-4;-4]; R1.r = 1; R1.obstacle = false; R1.rep = 'circular';
+        R2.c = [4;0];R2.r = 4;R2.obstacle = false; R2.rep = 'circular';
+        R3.c = [1;-4];R3.r = 2;R3.obstacle = false; R3.rep = 'circular';
 
         % Create a containers.Map
         predMap = containers.Map;
@@ -51,18 +51,21 @@ switch case_numb
 % case 2
     case 2
         st = ['G[0,1] F[2,3] mu1 and F[6,7] G[1,2] mu2' ...
-        ' and F[15,16](G[0,4] mu3 and F[1,4] mu1) and G[0,20] (not mu4)'];
-        predicateList = {'mu1', 'mu2', 'mu3', 'mu4'}';
-        R1.c = [-2;2]; R1.r = 1; R1.obstacle = false;
-        R2.c = [1.5;0.5];R2.r = 1;R2.obstacle = false;
-        R3.c = [-1;0];R3.r = 1.5;R3.obstacle = false;
-        R4.c = [0;1.5]; R4.r = 1;R4.obstacle = false;
+        ' and F[13,14](G[0,4] mu3 and F[1,4] mu1) and G[0,20] (not mu4) and F[15,20] mu5'];
+        predicateList = {'mu1', 'mu2', 'mu3', 'mu4','mu5'}';
+        R1.c = [-2;2]; R1.r = 1; R1.obstacle = false; R1.rep = 'circular';
+        R2.c = [1.5;0.5];R2.r = 1;R2.obstacle = false; R2.rep = 'circular';
+        R3.c = [-1;0];R3.r = 1.5;R3.obstacle = false; R3.rep = 'circular';
+        R4.c = [0;1.5]; R4.r = 1;R4.obstacle = false; R4.rep = 'circular';
+        Square5.c = [-3; -1.5]; Square5.r = 0.5;Square5.obstacle = false;Square5.rep = 'square'; 
+               
         % Create a containers.Map
         predMap = containers.Map;
         predMap('mu1') = R1;
         predMap('mu2') = R2;
         predMap('mu3') = R3;
         predMap('mu4') = R4;
+        predMap('mu5') = Square5;
         
         if use_valueFunction
             grid_min = [-5; -5; -pi]; % Lower corner of computation domain
@@ -88,9 +91,16 @@ if use_valueFunction
     predValueMap = containers.Map;
     keyArray = keys(predMap);
     for i = 1:numel(keyArray)
+        if strcmp(predMap(keyArray{i}).rep,'circular')
         % [grid,data] = build_unicycleCBF_helper_cylinder(grid_min,grid_max,center,radius)
-        [grid,data] = build_unicycleCBF_helper_cylinder(grid_min,grid_max,predMap(keyArray{i}).c,predMap(keyArray{i}).r);
-        region.grid = grid; region.data = data;region.rep = 'grid-data';
+            [grid,data] = build_unicycleCBF_helper_cylinder(grid_min,grid_max,predMap(keyArray{i}).c,predMap(keyArray{i}).r);
+        else 
+            if strcmp(predMap(keyArray{i}).rep,'square')
+                [grid,data] = build_unicycleCBF_helper_rectangle(grid_min,grid_max,predMap(keyArray{i}).c,predMap(keyArray{i}).r);
+            end
+        end
+        region.grid = grid; region.data = data; region.rep = 'grid-data';
+        region.obstacle = predMap(keyArray{i}).obstacle;
         predValueMap(keyArray{i}) = region;
     end
 end
